91c936a0f6e1a1f72f2f40e1e4e470097d35a369
Wibu <100986373+wibusantun@users.noreply.github.com>
2025-07-07 08:29:58 +0700

    install.sh

--- install.sh
+++ install.sh
@@ -51,7 +51,7 @@ get_ssl() {
   sleep 0.5
 
   sudo bash -c "$(curl -sL https://raw.githubusercontent.com/erfjab/ESSL/master/essl.sh)" @ --install
-  essl "$email" "$domain" /certs/
+  essl gerobak@gmail.com vpn.gerobak.biz.id /certs/
 
   echo
   green_msg 'Getting SSL finished.'
